3196cbadac4dbf3979d57095cf47fe0f77784b18
João Miguel <joaomiguel22c@gmail.com>
2025-01-29 16:08:42 +0100

    Atulization

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
+require('dotenv').config();
+const path=require('path'); 
 const express=require('express');
 const app=express();
+const {engine}=require('express-handlebars');
+
+
+app.engine('handlebars', engine({defaultLayout: 'main', }));
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/', require('./routes/router'));  
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const app=require('./app');
 
 const PORT=process.env.PORT || 3000;
 
-app.listen(PORT,()=>{
-    console.log(`Servidor rodando na porta ${PORT}`);
+app.listen(3000,()=>{
+console.log('servidor rodando na porta 3000');
 });
